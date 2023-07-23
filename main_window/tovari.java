8609dff36ebdf616502fe358cb45f498cb1ec8f6
Damir <79251228+JohnWick-cloud@users.noreply.github.com>
2023-07-24 00:30:10 +0500

    add scrollview and textview in appbar

--- main_window/tovari.java
+++ main_window/tovari.java
@@ -9,8 +9,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.navigation.NavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class tovari extends AppCompatActivity {
 
@@ -25,7 +36,7 @@ public class tovari extends AppCompatActivity {
         myDrawerLayout.addDrawerListener(myToggle);
         myToggle.syncState();
         NavigationView myNavView = findViewById(R.id.tovari_navigation_view);
-
+        AppBarLayout AppBar = findViewById(R.id.app_bar_tovari);
         myNavView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -43,5 +54,38 @@ public class tovari extends AppCompatActivity {
                 return false;
             }
         });
+        try{
+            InputStream inputStream = getAssets().open("products.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            String json;
+
+            json = new String(buffer, StandardCharsets.UTF_8);
+            JSONArray jsonArray = new JSONArray(json);
+            int max = jsonArray.length();
+
+            for(int i = 0; i < max; i++){
+                JSONObject product_item = jsonArray.getJSONObject(i);
+                String name = product_item.getString("name");
+                int id = product_item.getInt("id");
+
+                TextView product = new TextView(this);
+                product.setText("Имя продукта: " + name + "\n"+"ID продукта: " + id);
+                product.setId(i);
+                product.setTextSize(18);
+                product.setPadding(35, 35, 35, 35);
+                AppBar.addView(product);
+
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (JSONException js){
+            js.printStackTrace();
+        }
+
     }
+    
 }
